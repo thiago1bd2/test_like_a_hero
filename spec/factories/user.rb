667ea2803c85e1c7b3919a2a72cbb55dbc1869f0
thiago1bd2 <thiagodaqui@gmail.com>
2022-01-19 18:00:24 -0300

    testing some scenario with the model User

---
+++ spec/factories/user.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+FactoryBot.define do
+  factory :user do
+    nickname { FFaker::Lorem.word }
+    level { FFaker::Random.rand(1..99) }
+    kind { %i[knight wizard].sample }
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,18 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid if the level is not between 1 and 99' do
+    user = build(:user, level: FFaker::Random.rand(1..99_999))
+    expect(user).not_to be_valid
+  end
+
+  it 'returns the correct hero title' do
+    nickname = FFaker::Name.first_name
+    kind = %i[knight wizard].sample
+    level = FFaker::Random.rand(1..9)
+    user = create(:user, nickname: nickname, kind: kind, level: level)
+    expect(user.title).to eq("#{kind} #{nickname} ##{level}")
+  end
 end
